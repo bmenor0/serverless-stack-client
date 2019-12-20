98c9197eadf279d4658611a496c49c9375bdca09
bmenor0 <58092673+bmenor0@users.noreply.github.com>
2019-12-20 15:59:16 -0800

    Setting up our React app

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-upload-bmenor"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "  POST - https://npch9mhd1l.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_SyfpapSUT",
+      APP_CLIENT_ID: "26k5v6qurc11u7alpobj0n61ad",
+      IDENTITY_POOL_ID: "us-east-1:1f78a9fe-ad5a-40f6-ab99-ed4619250386"
+    }
+  };
