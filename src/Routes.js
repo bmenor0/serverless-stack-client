0c989caddadc2f5ce122279d3cd52c3a87506a9a
bmenor0 <58092673+bmenor0@users.noreply.github.com>
2019-12-26 13:35:18 -0800

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,55 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import Settings from "./containers/Settings";
+import Billing from "./containers/Billing";
+import ChangePassword from "./containers/ChangePassword";
+import ChangeEmail from "./containers/ChangeEmail";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes"
 
-export default function Routes() {
-    return (
-        <Switch>
-            <Route path="/" exact component={Home} />
-            { /* Finally, catch all unmatched routes */}
-            <Route component={NotFound} />
-        </Switch>
-    );
+
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+      <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AuthenticatedRoute path="/billing" exact component={Billing} appProps={appProps} />
+      <AuthenticatedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AuthenticatedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+      <AuthenticatedRoute
+        path="/settings"
+        exact
+        component={Settings}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/settings/password"
+        exact
+        component={ChangePassword}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/settings/email"
+        exact
+        component={ChangeEmail}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
+        path="/login/reset"
+        exact
+        component={ResetPassword}
+        appProps={appProps}
+      />
+      { /* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
+    </Switch>
+  );
 }
